91f2acfe8842230117bee22711e2b2d62cbb4618
SHAHILA SULTHANA  B <167762761+Shahilasulthana@users.noreply.github.com>
2025-08-24 21:33:35 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[m+i] = nums2[i];
-        }
-        Arrays.sort(nums1);
+        //Initilize 3 pointers namely i,j and k each starts their iteration with last the size of the nums1 array is equal to the sum of m and n
+        int i = m - 1;
+        int j = n-1;
+        int k = m + n -1;
 
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){ // If the element i is greater then add it to the end and decrement i
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j >= 0){ //if j has still elements left out
+            nums1[k--] = nums2[j--];
+        }
     }
 }
+
