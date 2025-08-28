3ea67a27704ea7ebce230a39d080737bd42398b4
SHAHILA SULTHANA  B <167762761+Shahilasulthana@users.noreply.github.com>
2025-08-28 23:41:23 +0530

    Time: 4 ms (50.21%), Space: 53.9 MB (8.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,7 +2,10 @@ class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        
-         return nums[n/2];
+        return nums[n/2];
     }
 }
+
+/** 
+The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted. Therefore, we can sort the array and return the element at index n/2.
+**/
