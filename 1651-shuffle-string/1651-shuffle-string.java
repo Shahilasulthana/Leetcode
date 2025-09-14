cf71b92fcbc5e155fa699ab68aaeb4eca89af2a2
SHAHILA SULTHANA  B <167762761+Shahilasulthana@users.noreply.github.com>
2025-09-14 23:48:07 +0530

    Time: 1 ms (68.37%), Space: 44.6 MB (77.82%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.java
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -1,14 +1,11 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        int len=indices.length;
-        Map<Integer,Character>map=new HashMap<>();
-        for(int i=0;i<len;i++){
-            map.put(indices[i],s.charAt(i));
+        char[] arr = new char[indices.length];
+
+        for(int i=0; i<indices.length; i++){
+            arr[indices[i]] = s.charAt(i);
         }
-        String res="";
-        for(int i=0;i<len;i++){
-            res+=map.get(i);
-        }
-            return res;
+        String res = new String(arr);
+        return res;
     }
 }
