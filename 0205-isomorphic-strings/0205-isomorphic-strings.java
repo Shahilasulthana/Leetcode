9e1cd149c53b42d14072a9de257738b78ad8f241
SHAHILA SULTHANA  B <167762761+Shahilasulthana@users.noreply.github.com>
2025-09-15 23:47:34 +0530

    Time: 23 ms (12.44%), Space: 42.7 MB (36.77%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Integer> charIndexS = new HashMap<>();
+        HashMap<Character, Integer> charIndexT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!charIndexS.containsKey(s.charAt(i))) {
+                charIndexS.put(s.charAt(i), i);
+            }
+
+            if (!charIndexT.containsKey(t.charAt(i))) {
+                charIndexT.put(t.charAt(i), i);
+            }
+
+            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
+                return false;
+            }
+        }
+
+        return true;        
+    }
+}
+
+// the approach here is if we encounter a character only once it will be whatever new char
+
+        // But if any character is present twice in the string s we have to check whether the particular character is replaced by the same character everywhere
