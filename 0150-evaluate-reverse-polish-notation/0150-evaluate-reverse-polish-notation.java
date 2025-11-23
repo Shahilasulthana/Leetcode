abd5c8c3b504bee781958fe8469d34519d36e63a
Shahila <167762761+Shahilasulthana@users.noreply.github.com>
2025-11-23 23:42:44 +0530

    Time: 8 ms (12.27%), Space: 45.1 MB (41.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -5,15 +5,16 @@ class Solution {
             if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
                 int b = st.pop();
                 int a = st.pop();
-            
-            int result = 0;
-            switch(token){
-                case "+" : result = a + b; break;
-                case "-" : result = a - b; break;
-                case "*" : result = a * b; break;
-                case "/" : result = a / b; break;
-            }
-            st.push(result);
+
+                int result = 0;
+
+                switch(token){
+                    case "+" : result = a + b; break;
+                    case "-" : result = a - b; break;
+                    case "*" : result = a * b; break;
+                    case "/" : result = a / b; break;
+                }
+                st.push(result);
             }
             else{
                 st.push(Integer.parseInt(token));
@@ -22,10 +23,3 @@ class Solution {
         return st.pop();
     }
 }
-
-// Reversed Polish Notation is a mathematical expression to express numbers without brackets
-// We use a stack to solve the RPN equations
-// What we do is :
-// If (number) => push 
-// If (operator) => pop 2 top numbers => result => push the result into the stack again
-// return the result
