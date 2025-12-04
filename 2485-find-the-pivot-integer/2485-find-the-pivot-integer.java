539f6de2f3a02b80d643238f8593fa2e3ff6e9e8
Shahila <167762761+Shahilasulthana@users.noreply.github.com>
2025-12-04 23:21:29 +0530

    Time: 0 ms (100%), Space: 42.4 MB (33.78%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,16 +1,18 @@
 class Solution {
     public int pivotInteger(int n) {
-        int total = n*(n+1) / 2;
+        int total = n * (n+1) / 2;
         int x = (int) Math.sqrt(total);
         if(x * x == total){
             return x;
         }
         return -1;
-
     }
 }
 
 // Compute total = n(n+1)/2
 // Check if total has a perfect square
 // If yes → sqrt(total) is pivot
-// Else → no pivot (-1)
+// Else → no pivot 
+
+// total = 8 * 9 / 2 = 36
+// x = sqrt(36) = 6 → perfect square
