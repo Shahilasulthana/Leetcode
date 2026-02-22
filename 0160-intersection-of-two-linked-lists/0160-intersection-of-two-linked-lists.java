1360d92380e7f535ec4f8de8dc6a539959d1fbd1
Shahila <167762761+Shahilasulthana@users.noreply.github.com>
2026-02-22 21:56:34 +0530

    Time: 1 ms (99.89%), Space: 52.9 MB (62.2%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        if(headA == null || headB == null) return null;
+        ListNode list1 = headA;
+        ListNode list2 = headB;
+
+        while(list1 != list2){
+            list1 = (list1 == null) ? headB : list1.next;
+            list2 = (list2 == null) ? headA : list2.next;
+        }
+        return list1;
+    }
+}
