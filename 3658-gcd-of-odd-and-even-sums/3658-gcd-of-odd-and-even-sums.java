dd1aa9976b0d46a801d4986db846514f14362a50
Shahila <167762761+Shahilasulthana@users.noreply.github.com>
2026-07-16 00:39:58 +0530

    Time: 0 ms (100%), Space: 42.4 MB (86.19%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
+
+// sum of odd numbers = n^2
+// sum of even numbers = n(n+1) = n^2 + n
+// GCD(n^2, n^2 + n)
+// n GCD(n, n+1)
+// GCD of two consecutive numbers is always 1
+// n (1) 
+// n
